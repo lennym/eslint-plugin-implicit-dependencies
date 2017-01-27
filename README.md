075ecfed8a382b990fcbab8bf961bda6b03d4d6c
Leonard Martin <leonard.martin@gmail.com>
2017-01-27 10:03:23 +0000

    Implementation of eslint plugin

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # eslint-plugin-implicit-dependencies
+
 eslint plugin to detect implicit dependencies
+
+Detects when a module has been required that is not listed as a dependency in the project's package.json.
+
+This helps prevent accidentally depending on a module that is present in node_modules as a result of being installed further down your dependency tree, but is not listed as an explicit dependency of your project.
+
+## Usage
+
+Add `implicit-dependencies` to the plugins section of your [ESLint configuration file](http://eslint.org/docs/user-guide/configuring#configuration-file-formats). You can omit the `eslint-plugin-` prefix:
+
+```yaml
+plugins:
+  - implicit-dependencies
+```
+
+Then configure the plugin under the rules section.
+
+```yaml
+rules:
+  - implicit-dependencies/no-implicit-dependencies: error
+```
+
+## Options
+
+By default `implicit-dependencies` will only look for dependencies in the `dependencies` section of your package.json. You can include dev, peer and optional dependencies by configuring the rule to include those sections as follows:
+
+
+```yaml
+rules:
+  - implicit-dependencies/no-implicit-dependencies:
+    - error
+    - dev: true
+      peer: true
+      optional: true
+```
+
+Or if configuring with javascript:
+
+```javascript
+rules: {
+  'implicit-dependencies/no-implicit-dependencies': [
+    'error',
+    { peer: true, dev: true, optional: true }
+  ]
+}
+```
---
+++ rules/no-implicit-dependencies.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const path = require('path');
+const findup = require('findup');
+const builtin = require('builtin-modules').reduce((map, key) => {
+  map[key] = true;
+  return map;
+}, {});
+
+module.exports = {
+  meta: {
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          optional: {
+            type: 'boolean'
+          },
+          peer: {
+            type: 'boolean'
+          },
+          dev: {
+            type: 'boolean'
+          }
+        },
+        additionalProperties: false
+      }
+    ]
+  },
+  create: (context) => {
+    return {
+      'CallExpression:exit': (node) => {
+        if (node.callee.name === 'require') {
+          let moduleName;
+          const name = node.arguments[0].value;
+
+          if (name[0] !== '.' && name[0] !== '/') {
+            // if module is a node core module then skip
+            if (builtin[name]) {
+              return;
+            }
+            // parse module name from scope packages and deep requires
+            if (name[0] === '@') {
+              moduleName = name.split('/').slice(0, 2).join('/');
+            } else {
+              moduleName = name.split('/')[0];
+            }
+
+            // find the nearest package.json
+            const dir = path.dirname(context.getFilename());
+            const jsonPath = path.join(findup.sync(dir, 'package.json'), 'package.json');
+            const pkg = require(jsonPath);
+
+            // check dependencies
+            const opts = context.options[0] || {};
+            console.log(opts);
+            if (pkg.dependencies[moduleName]) {
+              return;
+            } else if (pkg.optionalDependencies && pkg.optionalDependencies[moduleName] && opts.optional) {
+              return;
+            } else if (pkg.peerDependencies && pkg.peerDependencies[moduleName] && opts.peer) {
+              return;
+            } else if (pkg.devDependencies && pkg.devDependencies[moduleName] && opts.dev) {
+              return;
+            } else {
+              context.report({
+                node,
+                message: `Module "${moduleName}" is not listed as a dependency in package.json`
+              });
+            }
+          }
+        }
+      }
+    };
+  }
+};
